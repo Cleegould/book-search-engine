6eecfde734b9abc770d0987914256774f5bd8544
chrisg <cleegould@gmail.com>
2023-06-26 19:02:29 -0400

    corrected bugs and flaws in code

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Row, Container, CardGroup, Card, Button } from 'react-bootstrap';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-import { useQuery, useMutation } from '@apollo/client';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import { Row, Container, Col, Card, Button } from "react-bootstrap";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
+  const [userData, setUserData] = useState({});
 
-  const userData = data?.me || {};
+  useEffect(() => {
+    if (data) {
+      setUserData(data.me);
+    }
+  }, [data]);
 
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -20,11 +26,9 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await removeBook({ variables: { bookId } });
-      console.log('Deleted record: ', response);
-      if (error) {
-        console.log(error);
-      }
+      await removeBook({
+        variables: { bookId },
+      });
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
@@ -35,9 +39,13 @@ const SavedBooks = () => {
     return <h2>LOADING...</h2>;
   }
 
+  if (!Auth.loggedIn()) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <>
-      <Row fluid className='text-light bg-dark'>
+      <Row fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -46,29 +54,34 @@ const SavedBooks = () => {
         <h2>
           {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
+                userData.savedBooks.length === 1 ? "book" : "books"
               }:`
-            : 'You have no saved books!'}
+            : "You have no saved books!"}
         </h2>
-        <CardGroup>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? (
-                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardGroup>
+        <Col>
+          {userData.savedBooks.map((book) => (
+            <Card key={book.bookId} border="dark">
+              {book.image && (
+                <Card.Img
+                  src={book.image}
+                  alt={`The cover for ${book.title}`}
+                  variant="top"
+                />
+              )}
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <p className="small">Authors: {book.authors}</p>
+                <Card.Text>{book.description}</Card.Text>
+                <Button
+                  className="btn-block btn-danger"
+                  onClick={() => handleDeleteBook(book.bookId)}
+                >
+                  Delete this Book!
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
       </Container>
     </>
   );
